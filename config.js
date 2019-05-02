c3a803c79bb58cc050729cd1a2d667136a2be536
Karol Vargas <vargask@allegheny.edu>
2019-05-02 04:10:57 -0400

    post

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 module.exports = {
-    title: 'Karol Vargas', // Required
-    author: 'Karol Alberto Vargas', // Required
-    tagline: 'Miami, FL Full-Stack Developer',
-    primaryColor: '#3498db', // Required
-    showHeaderImage: true,
-    showShareButtons: true,
-    postsPerPage: 5, // Required
-    social: {
-        github: 'https://github.com/karolvargas',
-        twitter: 'https://twitter.com/karolavargas',
-        linkedin: 'https://www.linkedin.com/in/karolalbertovargas/'
-    }
+  title: 'Portfolio Blog', // Required
+  author: 'Karol Vargas', // Required
+  description: 'Aspiring Data Scientist',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    github: 'https://github.com/karolvargas',
+    linkedin: 'https://linkedin.com/in/karolalbertovargas',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
 };
